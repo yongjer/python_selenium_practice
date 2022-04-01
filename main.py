fc664ad1ca186d246b317cc06c8f32e6733e6f3a
yongjer <54315206+yongjer@users.noreply.github.com>
2022-04-01 13:15:22 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
 chromedriver = "/Users/yongjer/Downloads/chromedriver"
 
@@ -14,15 +15,15 @@ driver = webdriver.Chrome(service=s, options=option)
 
 driver.get("https://cu.nsysu.edu.tw/mooc/login.php")
 
-id = driver.find_element_by_id("s_username")
+id = driver.find_element(By.ID,"s_username")
 
 id.send_keys("YourId")
 
-passward = driver.find_element_by_name("password")
+passward = driver.find_element(By.NAME,"password")
 
 passward.send_keys("YourPassword")
 
-send = driver.find_element_by_id("btnSignIn")
+send = driver.find_element(By.ID,"btnSignIn")
 
 send.click()
 
